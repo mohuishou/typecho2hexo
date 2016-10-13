060f7b758f018b930be67621a6de288e09e12406
mohuishou <1@lailin.xyz>
2016-10-13 22:52:53 +0800

    完善注释，优化代码

--- Lib/Convert.php
+++ Lib/Convert.php
@@ -2,9 +2,21 @@
 namespace Mohuishou\Lib;
 
 class Convert{
-
+    /**
+     * config对象
+     * @var Config
+     */
     protected $_config;
+
+    /**
+     * Attachment对象
+     * @var Attachment
+     */
     protected $_attachment;
+
+    /**
+     * Convert constructor.
+     */
     public function __construct()
     {
         $this->_config=Config::getInstance();
@@ -14,9 +26,10 @@ class Convert{
     }
 
     /**
-     *
+     * 从数据库获取数据并且做出相关处理
      */
     protected function getData(){
+        //从数据库取出数据
         $db=$this->_config->get("db");
         try {
             $dhn = new \PDO($db['dsn'], $db["user"], $db["password"]);
@@ -32,24 +45,24 @@ TEXT;
         }catch (\PDOException $e){
             die($e->errorInfo);
         }
-
+        //初始化ATTACHMENT对象
         $is_download=$this->_config->get("attachment")["is_download"];
         $is_download && $this->_attachment=new Attachment();
 
         foreach ($rows as $row){
-
             //字符编码转换
             if($this->_config->get("is_gbk")){
                 $row=$this->gbk2uft8($row);
             }
+
             $created_time=date('Y-m-d H:i:s',$row["created"]);
             $content=str_replace('<!--markdown-->','',$row["text"]);
             $filename=str_replace(array(" ","?","\\","/" ,":" ,"|", "*" ),'-',$row["title"]);
 
+            //附件下载
             if($is_download){
                 $content=$this->attachment($filename,$content);
             }
-
             $temp=<<<EOT
 title: {$row["title"]}
 categories: {$row["category"]}
@@ -59,7 +72,7 @@ date: {$created_time}
 
 {$content}
 EOT;
-
+            //保存文档
             file_put_contents(__DIR__."/../FILE/".$filename.".md",$temp);
         }
     }
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 require_once './vendor/autoload.php';
-new \Mohuishou\Lib\Convert();
+try {
+    new \Mohuishou\Lib\Convert();
+}catch (Exception $e){
+    echo "\r\n error： ".$e->getMessage();
+}
 
