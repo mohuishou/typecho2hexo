55021ce4dfaa2fec9df2106095de47fa08a0fec0
mohuishou <1@lailin.xyz>
2016-10-13 20:17:43 +0800

    update

--- config.example.php
+++ config.example.php
@@ -1,15 +1,24 @@
 <?php
 return [
-
-    "db_host"=>"localhost",
-    "db_port"=>3306,
-    "db_name"=>"typecho",
-    "db_user"=>"root",
-    "db_password"=>"",
-
-    "is_gbk"=>false,
+    "db"=>[
+        "host"=>"localhost", //数据库地址
+        "port"=>3306, //端口号
+        "name"=>"typecho", //数据库
+        "user"=>"root", //数据库用户名
+        "password"=>"", //数据库密码
+        "prefix"=>"typecho"  //表前缀
+    ],
+    "is_gbk"=>false, //是否开启gbk转utf8（有些数据库里面储存的不是utf8格式需要开启这个选项）
+    //附件相关
     "attachment"=>[
-        "is_download"=>false,
-        "download_type"=>"file",
+        "is_download"=>true, //是否下载附件
+        "type"=>"file" //附件保存类型：file 或者 qiniu
+    ],
+    //七牛云储存相关
+    "qiniu"=>[
+        "access_key"=>"",
+        "secret_key"=>"",
+        "bucket_name"=>"blog", //七牛空间名
+        "domain"=>"http://" //七牛外链域名，必须设置,带http://
     ]
 ];
