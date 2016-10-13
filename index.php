b876f79d048e4b54b5a0cd900c4cc1511621ce98
mohuishou <1@lailin.xyz>
2016-10-13 20:18:53 +0800

    upadte

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
-use Dotenv\Dotenv;
-use Mohuishou\Lib\Config;
 require_once './vendor/autoload.php';
-//$a=require_once "./config.php";
 new \Mohuishou\Lib\Convert();
-//$a=new \Mohuishou\Lib\Attachment();
-//$a->saveQiniu();
-//print_r($a);
-//$dotenv=new Dotenv(__DIR__);
-//$dotenv->load();
-//echo getenv("DB");
-//$c = new Config();
+
